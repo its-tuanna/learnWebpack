38b706a2c04031ae0174322482a7500b9758a1c7
its-tuanna <tuanna@its-global.vn>
2022-07-04 10:42:03 +0700

    add image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { helloWorld } from "./exportFunc";
 import "./styles/main.scss";
+import earthSvgSrc from "./assets/earth-globe-svgrepo-com.svg";
 
 console.log(helloWorld());
+
+const earthImg = document.getElementById("earth-img");
+earthImg.src = earthSvgSrc;
