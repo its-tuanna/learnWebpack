54552e2924ca6106d86de925914b3d142c5b6e4e
its-tuanna <tuanna@its-global.vn>
2022-07-04 10:56:55 +0700

    add webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
@@ -64,5 +66,6 @@ module.exports = {
       filename: "index.html",
       template: "src/template.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
